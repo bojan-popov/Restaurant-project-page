74bba9a3c658d01559d8e30fd1ba22145adb9579
Bojan Popov <bp2307994450145@gmail.com>
2021-03-17 23:37:58 -1200

    createing routes for dimanic page

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Nav.css";
+
 function Nav() {
   return (
     <>
-      <ul>
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <Link to="/about">
-          <li>About</li>
-        </Link>
-        <Link to="/menu">
-          <li>Menu</li>
-        </Link>
-        <Link to="/contact">
-          <li>Contact</li>
-        </Link>
-        <Link to="/location">
-          <li>Location</li>
-        </Link>
-      </ul>
+      <div className="navMainDiv">
+        <ul className="navUl">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/menu">
+            <li>Menu</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+          <Link to="/location">
+            <li>Location</li>
+          </Link>
+        </ul>
+      </div>
     </>
   );
 }
